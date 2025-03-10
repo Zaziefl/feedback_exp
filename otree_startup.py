d87c5e8d98d52c086c15e6bb82220f4c3c9c4086
Zazie Flerackers <zazieflerackers@Air-van-Zazie.home>
2025-03-10 12:04:28 +0100

    Fix settings module path

--- otree_startup.py
+++ otree_startup.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from otree.asgi import get_asgi_application
+
+# Ensure Heroku finds the project folder
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('OTREE_ENV', 'production')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedback_exp_buyer.settings')
